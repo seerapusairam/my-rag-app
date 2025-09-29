a39962385ae4fc8b43be1823ecc0ff9b8db09f9d
seerapusairam <seerapu.sai123@gmail.com>
2025-09-29 17:31:50 -0400

    Updated app to call the router

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import 'dotenv/config'; 
 import {router} from './router/askRouter.js'
+import { setupRag } from './setupRag.js'
 
 const app = express();
 const port = process.env.PORT
 
 app.use(express.json());
 
-app.post('/ask', router);
+app.use('/ask', router);
 
-app.listen(port, () => {
-    console.log(`Server running at ${port}`);
-});
+const startServer = async () => {
+    await setupRag();
+    
+    app.listen(port, () => {
+        console.log(`Server running at ${port}`);
+    });
+}
+
+startServer();
